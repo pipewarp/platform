7e718c4b4c629bc7c735d2364f518c5cc9b7585f
Andrew Mott <andrew@andrewmott.com>
2025-10-18 02:55:10 -0400

    test: add example streaming mcp servers and clients (#67)

---
+++ examples/mcp-servers/transform.server.ts
@@ -0,0 +1,172 @@
+import express from "express";
+import { z } from "zod";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+
+const app = express();
+app.use(express.json());
+
+// map transports to specific session ids (no auth)
+const sessions = new Map<string, SSEServerTransport>();
+const port = 3005;
+const mcp = new McpServer({
+  name: "transform-server",
+  version: "0.1.0-alpha.1",
+  capabilities: { logging: {} },
+});
+
+// transform characters to another character
+const characterSwap = new Map<string, string>([
+  ["🟦", "🟥"],
+  ["🟧", "🟨"],
+  ["🟥", "🟩"],
+  ["⬛️", "🟩"],
+  ["🟫", "🟩"],
+]);
+
+const TransformOutputSchema = z.object({
+  ok: z.boolean(),
+  data: z.string().optional(),
+  message: z.string().optional(),
+});
+type TranformOutput = z.infer<typeof TransformOutputSchema>;
+
+mcp.registerTool(
+  "transform",
+  {
+    title: "transform",
+    description: "converts unicode characters to an alternate color",
+    inputSchema: {
+      delayMs: z.number(),
+      art: z.string(),
+      isStreaming: z.boolean(),
+    },
+    outputSchema: {
+      ok: z.boolean(),
+      data: z.string().optional(),
+      message: z.string().optional(),
+    },
+  },
+  async ({ delayMs, art, isStreaming }, ctx) => {
+    const { sessionId } = ctx;
+    console.log("[transform-server] sessionId:", sessionId);
+
+    console.log("client sent art:", art);
+
+    if (!sessionId) {
+      console.log("[transform-server] no session");
+      const output: TranformOutput = {
+        ok: false,
+        message: "no sessionId provided",
+      };
+      return {
+        content: [{ type: "text", text: JSON.stringify(output) }],
+        structuredContent: output,
+      };
+    }
+    const transport = sessions.get(sessionId);
+
+    if (!sessions.has(sessionId) || transport === undefined) {
+      const output: TranformOutput = { ok: false, message: "invalid session" };
+      return {
+        content: [{ type: "text", text: JSON.stringify(output) }],
+        structuredContent: output,
+      };
+    }
+
+    if (!isStreaming) {
+      let newArt = "";
+
+      const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
+      const chars = [...segmenter.segment(art)].map((seg) => seg.segment);
+
+      for (const char of chars) {
+        newArt += characterSwap.get(char) ?? char;
+      }
+      const output: TranformOutput = {
+        ok: true,
+        data: newArt,
+      };
+      return {
+        content: [{ type: "text", text: JSON.stringify(output) }],
+        structuredContent: output,
+      };
+    }
+
+    // should move this out eventually as own thing, but this is just a demo
+    (async () => {
+      if (!isStreaming) return;
+      await new Promise((r) => setTimeout(r, delayMs));
+      let newArt = "";
+
+      const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
+      const chars = [...segmenter.segment(art)].map((seg) => seg.segment);
+
+      for (const char of chars) {
+        newArt += characterSwap.get(char) ?? char;
+      }
+
+      console.log("transform-server] sending:", art);
+      await transport.send({
+        jsonrpc: "2.0",
+        method: "notifications/message",
+        params: {
+          level: "info",
+          message: newArt,
+        },
+      });
+    })();
+
+    return {
+      content: [{ type: "text", text: art }],
+      structuredContent: { ok: true },
+    };
+  }
+);
+
+// endpoint for created th sse connection
+app.get("/sse", async (_req, res) => {
+  console.log("[transform-server] connecting /sse");
+  const transport = new SSEServerTransport("/messages", res);
+  sessions.set(transport.sessionId, transport);
+
+  res.on("close", () => {
+    sessions.delete(transport.sessionId);
+    transport.close();
+  });
+
+  await mcp.connect(transport);
+});
+
+// endpoint for handling other non SSE stuff like tool calls
+// POST /messages or /messages/:sessionId JSON-RPC calls
+app.post(["/messages", "/messages/:sessionId"], async (req, res) => {
+  console.log("[transform-server] received messages post request");
+
+  // /messages/:sessionId
+  const paramId = (req.params as any)?.sessionId as string | undefined;
+  // /messages?sessionId=
+  const queryId = (req.query?.sessionId as string) || undefined;
+  const headerId = (req.header("mcp-session-id") as string) || undefined;
+
+  const sessionId = paramId || queryId || headerId || "";
+  const transport = sessions.get(sessionId);
+
+  if (!transport) {
+    console.error(
+      "[transform-server] Error; No transport for sessionId:",
+      sessionId,
+      "path:",
+      req.path
+    );
+    return res.status(400).send("No transport for sessionId");
+  }
+
+  await transport.handlePostMessage(req, res, req.body);
+});
+
+app.listen(port, () => {
+  console.log("[transform-server] ascii MCP SSE server running.");
+  console.log(`[transform-server] Listening on http://localhost:${port}`);
+  console.log(`[transform-server] GET /sse, POST /messages`);
+});
---
+++ examples/mcp-servers/unicode.client.ts
@@ -0,0 +1,28 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
+import { LoggingMessageNotificationSchema } from "@modelcontextprotocol/sdk/types.js";
+
+const client = new Client({ name: "unicode-client", version: "0.1.0-alpha.1" });
+const transport = new SSEClientTransport(new URL("http://localhost:3004/sse"));
+console.log("[unicode-client] running");
+await client.connect(transport);
+
+let art = "";
+const stream = false;
+client.setNotificationHandler(LoggingMessageNotificationSchema, (n) => {
+  if (stream) {
+    console.log("[unicode-client] log message arg:", n);
+    console.log(n.params.message);
+    console.log(`[unicode-client] [${n.params.level}] ${n.params.message}`);
+    art += n.params.message;
+    console.log(art);
+  } else {
+    console.log("[unicode-client] log message arg:", n);
+  }
+});
+
+const result = await client.callTool({
+  name: "draw",
+  arguments: { delayMs: 25, useStream: stream },
+});
+console.log("[unicode-client] draw tool result:", result.structuredContent);
