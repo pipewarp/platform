50fd1dbc778952ddee60964585068189502a4e75
Andrew Mott <andrew@andrewmott.com>
2025-11-13 09:21:15 -0500

    feat: init ui package with desktop integration

---
+++ packages/ui/src/core/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+export function AppTwo() {
+  return (
+    <div>
+      <p>Test of the core stuff.</p>
+    </div>
+  );
+}
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from "./core/App.js";
